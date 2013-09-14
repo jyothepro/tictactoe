b56ca13bc6fa2b1f0af3ad31e1af44d7d03987a3
Jyothidhar Pulakunta <cooljyo@gmail.com>
2013-09-14 12:26:30 -0700

    Adding view elements

--- TicTacToe/TicTacToeViewController.h
+++ TicTacToe/TicTacToeViewController.h
@@ -9,5 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface TicTacToeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *gameState;
+@property (weak, nonatomic) IBOutlet UIButton *startNewGameBtn;
+@property (weak, nonatomic) IBOutlet UIImageView *img11;
+@property (weak, nonatomic) IBOutlet UIImageView *img12;
+@property (weak, nonatomic) IBOutlet UIImageView *img13;
+@property (weak, nonatomic) IBOutlet UIImageView *img21;
+@property (weak, nonatomic) IBOutlet UIImageView *img22;
+@property (weak, nonatomic) IBOutlet UIImageView *img23;
+@property (weak, nonatomic) IBOutlet UIImageView *img31;
+@property (weak, nonatomic) IBOutlet UIImageView *img32;
+@property (weak, nonatomic) IBOutlet UIImageView *img33;
 
 @end
