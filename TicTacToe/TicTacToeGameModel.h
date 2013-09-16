ffee6d503a5e0ecb08c0915eadb235d0f9168d05
Jyothidhar Pulakunta <cooljyo@gmail.com>
2013-09-15 19:31:22 -0700

    Adding some comments for the game logic

--- TicTacToe/TicTacToeGameModel.h
+++ TicTacToe/TicTacToeGameModel.h
@@ -10,18 +10,21 @@
 #define NUM_ROWS 3
 #define NUM_COLS 3
 
+//Types of Icons on the game board
 typedef enum {
 	TYPE_NOUGHT,
 	TYPE_CROSS,
 	TYPE_EMPTY
 } IconType;
 
+//Skill level of the iDevice
 typedef enum {
 	SKILL_NOVICE = 0,
 	SKILL_AVERAGE = 2,
-	SKILL_EXPERT = 100,
+	SKILL_EXPERT = 10,
 } SkillLevel;
 
+//State of the game
 typedef enum {
 	STATE_CROSS_TURN,
 	STATE_NOUGHT_TURN,
@@ -30,17 +33,25 @@ typedef enum {
 	STATE_TIE
 } GameState;
 
+
 @interface TicTacToeGameModel : NSObject {
 	IconType gameBoard[NUM_ROWS][NUM_COLS];
 	GameState gameState;
 }
 
+//Current Game State
 @property (nonatomic) GameState gameState;
 
-- (IconType) getMarkInRow:(int) aRow column:(int) aColumn;
+// Set the move played by the player
 - (BOOL) playerPressedRow:(int) aRow column:(int) aColumn;
+
+//Reset the game board
 - (BOOL) resetBoard;
+
+//AI for making the iDevice move
 - (int) iPhoneMove;
+
+//Set the skill level of the AI (can be set inbetween the game too)
 - (void) setSkillLevel:(SkillLevel) val;
 
 @end
--- TicTacToe/TicTacToeGameModel.m
+++ TicTacToe/TicTacToeGameModel.m
@@ -22,24 +22,21 @@
 
 - (id)init {
 	if(self = [super init]){
-		self.gameState = STATE_CROSS_TURN;
+		//Reset the gameboard
 		[self resetBoard];
+		
+		//Set the skill level to average by default
 		[self setSkillLevel:SKILL_AVERAGE];
 	}
 	return self;
 }
 
+// Set the skill level of the AI (can be set inbetween the game too)
 - (void) setSkillLevel:(SkillLevel) val {
 	_skillLevel = val;
 }
 
-- (IconType) getMarkInRow:(int) aRow column:(int) aColumn {
-	if (aRow >= NUM_ROWS || aColumn >= NUM_COLS || aRow < 0 || aColumn < 0) {
-		return TYPE_EMPTY;
-	}
-	return gameBoard[aRow][aColumn];
-}
-
+// Update the move played by the player
 - (BOOL) playerPressedRow:(int) aRow column:(int) aColumn {
 	if (aRow >= NUM_ROWS || aColumn >= NUM_COLS ||
 		aRow < 0 || aColumn < 0 || gameBoard[aRow][aColumn] != TYPE_EMPTY) {
@@ -60,6 +57,7 @@
 	return false;
 }
 
+//Reset the gameboard state
 - (BOOL) resetBoard {
 	for (int i=0; i<NUM_ROWS; i++) {
 		for (int j=0; j<NUM_COLS; j++) {
@@ -70,6 +68,7 @@
 	return true;
 }
 
+// Check if there is a winner or if the game is a tie
 - (void) _checkForWinner {
 	if ([self _didWin:TYPE_CROSS]) {
 		NSLog(@"X won the game");
@@ -83,6 +82,7 @@
 	}
 }
 
+// Does every position on the gameboard is filled with an icon
 - (BOOL) _isBoardFull {
 	for (int i=0; i<NUM_ROWS; i++) {
 		for (int j=0; j<NUM_COLS; j++) {
@@ -93,13 +93,21 @@
 	}
 	return true;
 }
-/*
- Then play the move that blocks a winning line, or if there isn't one, play a move at random. 
- Perhaps weight the centre for 3 and the corners for 2, with the edges at 1, but if what you want is an opponent you can beat, you don't want to be to clever.
- */
+
+//AI for the iPhone move
 - (int) iPhoneMove {
-	int countCross = 0;
-	int countEmpty = 0;
+	/*
+	 AI Logic
+	 Based on probability 
+		Block the winning move of the opponent is present
+	 end;
+	 
+	 Make a random move based on weights
+	 Place the icon in the center if its empty
+	 Place the icon in the corners if any available; if multiple available pick one randomly
+	 Place the icon in the edges if any available; if multiple available pick one randomly
+	 
+	 */
 	
 	//Probability to block a winning move
 	if (arc4random_uniform(_skillLevel)) {
@@ -108,28 +116,6 @@
 		int blockWinningMove = [self blockOpponentWinningMove];
 		if (blockWinningMove) {
 			return blockWinningMove;
-		}
-	}
-	
-	
-	
-	// Check for up diagonal win
-	countCross = 0;
-	countEmpty = 0;
-	for (int i=0; i<NUM_ROWS; i++) {
-		if (gameBoard[NUM_ROWS-i-1][i] == TYPE_CROSS) {
-			countCross++;
-		} else if (gameBoard[NUM_ROWS-i-1][i] == TYPE_EMPTY) {
-			countEmpty++;
-		}
-	}
-	if (countCross == 2 && countEmpty == 1) {
-		if (gameBoard[2][0] == TYPE_EMPTY) {
-			return 7;
-		} else if (gameBoard[1][1] == TYPE_EMPTY) {
-			return 5;
-		} else {
-			return 3;
 		}
 	}
 	
@@ -188,6 +174,7 @@
 	return 0;
 }
 
+//Select if AI can block any winning move for the opponent
 -(int) blockOpponentWinningMove {
 	//Block winning row
 	int countCross, countEmpty;
@@ -234,7 +221,7 @@
 		}
 	}
 	
-	//Block winning diagonal
+	/*Block winning diagonals*/
 	// Check for down diagonal win
 	countCross = 0;
 	countEmpty = 0;
@@ -254,9 +241,31 @@
 			return 9;
 		}
 	}
+	
+	// Check for up diagonal win
+	countCross = 0;
+	countEmpty = 0;
+	for (int i=0; i<NUM_ROWS; i++) {
+		if (gameBoard[NUM_ROWS-i-1][i] == TYPE_CROSS) {
+			countCross++;
+		} else if (gameBoard[NUM_ROWS-i-1][i] == TYPE_EMPTY) {
+			countEmpty++;
+		}
+	}
+	if (countCross == 2 && countEmpty == 1) {
+		if (gameBoard[2][0] == TYPE_EMPTY) {
+			return 7;
+		} else if (gameBoard[1][1] == TYPE_EMPTY) {
+			return 5;
+		} else {
+			return 3;
+		}
+	}
+	
 	return 0;
 }
 
+//If icon type won the game
 - (BOOL) _didWin:(IconType) iconType {
 	BOOL marksMatchType;
 	int i,j;
@@ -305,12 +314,7 @@
 			marksMatchType = NO;
 		}
 	}
-	if (marksMatchType) {
-		return YES;
-	}
-	
-	// No winner
-	return NO;
+	return marksMatchType;
 }
 
 @end
